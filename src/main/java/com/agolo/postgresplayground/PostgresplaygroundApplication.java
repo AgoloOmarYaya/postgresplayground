97cf4cbd24f2023da71f0c4e36cd6b315fcd9b2e
omaryaya <omar.yaya.work@gmail.com>
2022-07-19 14:14:21 +0200

    Initial commit

---
+++ src/main/java/com/agolo/postgresplayground/PostgresplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.agolo.postgresplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresplaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agolo/postgresplayground/PostgresplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agolo.postgresplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
